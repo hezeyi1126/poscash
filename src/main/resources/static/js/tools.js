2ea7855a9c0e22f5fdd7456b8fd8f5e9f04778ce
hebaoming <1qaz@WSX>
2020-10-27 15:41:12 +0800

    修改ajax请求类型

--- src/main/resources/static/js/tools.js
+++ src/main/resources/static/js/tools.js
@@ -9,7 +9,28 @@
 	调用服务接口  service-服务名（springBean名）  method-调用的方法名   params-参数对象   , func 回调函数
 	*/
 	Pro.prototype.callServer= function(service ,method , params ,func){
-		axios({
+	
+		$.ajax({
+				url:"/serviceCtl" , // 请求路径
+				type:"POST" , //请求方式
+				contentType: "application/json; charset=utf-8",
+				data:JSON.stringify({
+					serviceName : service,
+					methodName : method,
+					params : params
+				}),
+				success:function (res) {
+					if(res.state == '-9'){
+						top.window.location='/login.html';
+					}
+					func(res);
+				},//响应成功后的回调函数
+				error:function () {
+					alert("出错啦...")
+				},//表示如果请求响应出现错误，会执行的回调函数
+				dataType:"json"//设置接受到的响应数据的格式
+			});
+		/*axios({
 			    method: 'post',
 			    url: '/serviceCtl',
 			    data:{
@@ -20,7 +41,7 @@
 		    }
 		).then(function(res){
 			func(res)
-		});
+		});*/
 	}
 	
 	
